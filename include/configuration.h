472ee8de693e1da30154de3f1fc922ff15c78d7b
daviddaguero <david.aguero@mi.unc.edu.ar>
2024-11-09 22:51:37 -0300

    Added functions to read temperature and to set the pwm duty cycle

--- include/configuration.h
+++ include/configuration.h
@@ -11,6 +11,7 @@
 #include <libopencm3/cm3/nvic.h> /**< Include the NVIC peripheral library */
 #include <libopencm3/stm32/timer.h> /**< Include the timer peripheral library */
 #include <libopencm3/stm32/i2c.h> /**< Include the I2C library */
+#include <libopencm3/stm32/adc.h> /**< Include the adc peripheral library */
 
 #define ALARM_PORT GPIOA /**< Alarm port corresponds to port A */
 #define ALARM_PIN GPIO5 /**< Define the alarm pin as PA5 */
@@ -81,7 +82,6 @@ static uint32_t duty_cycle = 0; /**< Initialize the duty cycle to 0 */
  * @brief Initializes the system clock to 72 MHz using an 8 MHz external crystal.
  */
 void system_clock_setup(void);
-=======
 
 #define ADC_CHANNEL_TEMP_SENSOR 0 /**< Timer uses ADC chanell 0 */
   
--- src/system_response.c
+++ src/system_response.c
@@ -1,1 +1,38 @@
+#include "system_response.h"
 
+uint16_t process_temperature(uint16_t value) 
+{
+    uint16_t temperature = (value / ADC_RESOLUTION) * TEMP_SCALE_FACTOR;
+
+    return temperature;
+}
+
+uint16_t read_temperature(void) {
+    // Configurar el canal especificado
+    adc_set_regular_sequence(ADC1, 1, ADC_CHANNEL_TEMP_SENSOR); // Secuencia con un solo canal
+
+    // Iniciar la conversión
+    adc_start_conversion_regular(ADC1);
+
+    // Esperar a que la conversión termine
+    while (!(ADC1_SR & ADC_SR_EOC));
+
+    // Leer y devolver el resultado de la conversión
+    return adc_read_regular(ADC1);
+}
+
+void set_pwm_duty_cycle(uint8_t duty_cycle, uint8_t output_channel) {
+    // Calculate the PWM compare value based on the duty cycle
+    uint32_t pwm_value = (TIMER_PERIOD * duty_cycle) / PERCENTAGE_MAX;
+
+    if(output_channel == PWM_CHANNEL_2)
+    {
+        // Set the duty cycle value for the output channel 2 (OC2) of TIM1
+        timer_set_oc_value(TIM1, TIM_OC2, pwm_value);
+    }
+    else
+    {
+        // Set the duty cycle value for the output channel 3 (OC3) of TIM1
+        timer_set_oc_value(TIM1, TIM_OC3, pwm_value);
+    }
+}

a2f23a3587e1914e4b4870d575978a292e2a55ce
Sofia Aldana Ávalos <108819650+sofiavalos@users.noreply.github.com>
2024-11-11 20:13:52 -0300

    Revert "Alarm and notification"

--- src/configuration.c
+++ src/configuration.c
@@ -94,6 +94,26 @@ void config_i2c(void)
 
     // Enable I2C to start communication
     i2c_peripheral_enable(I2C1);
+
+    // Enable clock for and I2C2
+    rcc_periph_clock_enable(RCC_I2C2);
+
+    // GPIO pins configuration for SDA and SCL 
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+              GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
+              GPIO_I2C2_SCL | GPIO_I2C2_SDA);
+
+    // Disable I2C1 before configurating it
+    i2c_peripheral_disable(I2C2);
+
+    // I2C basic configuration
+    i2c_set_clock_frequency(I2C2, I2C_CR2_FREQ_36MHZ);
+    i2c_set_standard_mode(I2C2);
+    i2c_set_trise(I2C2, I2C1_TRISE_100KHZ); // Rising time in standard mode
+    i2c_set_ccr(I2C2, I2C1_CCR_100KHZ);  // Set the CCR to 100 kHz
+
+    // Enable I2C to start communication
+    i2c_peripheral_enable(I2C2);
 }
 
 void config_pwm(void) 

649468fda12276cfd7917c1c5e948a22fdf6f323
Sofia Aldana Ávalos <108819650+sofiavalos@users.noreply.github.com>
2024-11-14 21:06:35 -0300

    Update configuration.h

--- include/configuration.h
+++ include/configuration.h
@@ -3,11 +3,14 @@
 #include "lpc17xx_exti.h"    /* Include the EXTI library */
 #include "lpc17xx_gpio.h"    /* Include the GPIO library */
 #include "lpc17xx_pinsel.h"  /* Include the PINSEL library */
+#include "lpc17xx_timer.h"   /* Include the TIMER library */
 
-#define MOTOR_NEG_PIN           ((uint32_t)(1 << 22)) /**< Assign port 0, pin 22 to the LED pin */
+#define MOTOR_NEG_PIN           ((uint32_t)(1 << 6)) /**< Assign port 0, pin 22 to the LED pin */
 #define MOTOR_POS_PIN         ((uint32_t)(1 << 3))  /**< Assign port 0, pin 3 to the motor pin */
 #define MANUAL_SWITCH_PIN ((uint32_t)(1 << 13)) /**< Assign port 2, pin 12 to the manual switch pin */
 #define OVERRIDE_SWITCH_PIN   ((uint32_t)(1 << 12)) /**< Assign port 2, pin 13 to the door switch pin */
+#define ALARM_PIN           ((uint32_t) (1<<2)) /**< Assign port 0 pin 2 to the alarm pin */
+#define FIRE_SENSOR_PIN   ((uint32_t) (1<<10))
 
 #define INPUT  0 /**< Macro to define the input */
 #define OUTPUT 1 /**< Macro to define the output */
@@ -18,6 +21,11 @@
 #define HIGH_PRIORITY 0 /**< Define the high priority */
 #define MID_PRIORITY  1 /**< Define the low priority */
 #define LOW_PRIORITY  2 /**< Define the low priority */
+
+#define PRESCALE_VALUE 1000 /**< Define the prescale value */
+#define MATCH_CHANNEL 0
+#define MATCH_VALUE 5000
+
 
 /**
  * @brief Configures the ports for the temperature sensor, switch, LED and motor
@@ -32,4 +40,7 @@ void config_ports(void);
  * @return void
  */
 void config_eint(void);
+
+
+void config_timer_and_match(void);
 

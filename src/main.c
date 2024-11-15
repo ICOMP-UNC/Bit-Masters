0307dfa0fd20953a82b3a303d400ff1eec3e1f74
Sofia Aldana Ávalos <108819650+sofiavalos@users.noreply.github.com>
2024-11-14 21:04:55 -0300

    Update main.c
    Add alarm handlers

--- src/main.c
+++ src/main.c
@@ -15,56 +15,64 @@
 #include <cr_section_macros.h> /* The cr_section_macros is specific to the MCUXpresso delivered toolchain */
 #endif
 
-#include "lpc17xx_systick.h" /* Include the SYSTICK library */
-#include "lpc17xx_timer.h"   /* Include the TIMER library */
-
 #include "configuration.h"
 
-/**
- * @brief External interrupt handler
- *
- * If the external interrupt 2 is triggered, disable the external interrupt 0 and 1
- * If the external interrupt 0 and 1 are triggered, enable the external interrupt 0 and 1
- *
- * @return void
- */
+void EINT0_IRQHandler(void)
+{
+    EXTI_ClearEXTIFlag(EXTI_EINT0); /* Clear the external interrupt flag */
+
+    if(LPC_SC->EXTPOLAR & (1 << 0))
+    {
+        // A rising edge has occured
+        // Fire has been detected
+        // Config falling edge
+        LPC_GPIO0->FIOSET |= MOTOR_NEG_PIN; /* Turn on the motor to close the door */
+        LPC_GPIO0->FIOSET |= ALARM_PIN; /* Trigger the alarm */
+        EXTI_SetPolarity(EXTI_EINT0, EXTI_POLARITY_LOW_ACTIVE_OR_FALLING_EDGE);
+    }
+    else
+    {
+        // A falling edge has occured
+        // Fire has been extinguished
+        // Config rising edge
+        LPC_GPIO0->FIOCLR |= ALARM_PIN; // Deactivate the alarm
+        LPC_GPIO0->FIOCLR |= MOTOR_NEG_PIN;
+        EXTI_SetPolarity(EXTI_EINT0, EXTI_POLARITY_HIGH_ACTIVE_OR_RISING_EDGE);
+    }
+}
+
+
+
+void TIMER0_IRQHandler(void)
+{
+    if (TIM_GetIntStatus(LPC_TIM0, TIM_MR0_INT) == SET)
+    {
+    	TIM_ClearIntPending(LPC_TIM0, TIM_MR0_INT);
+
+        if(LPC_GPIO0->FIOPIN & (MOTOR_POS_PIN || MOTOR_NEG_PIN))
+        {
+            LPC_GPIO0->FIOCLR |= MOTOR_POS_PIN; /* Turn off the motor */
+            LPC_GPIO0->FIOCLR |= MOTOR_NEG_PIN; /* Turn on the motor */
+        }
+    }
+}
+
 void EINT2_IRQHandler(void)
 {
     EXTI_ClearEXTIFlag(EXTI_EINT2); /* Clear the external interrupt flag */
 
     if(LPC_SC->EXTPOLAR & (1 << 2))
     {
-        // desactivar interrupciones
-        // Configurar flanco descendente
     	NVIC_EnableIRQ(EINT3_IRQn);
+    	NVIC_DisableIRQ(EINT0_IRQn);
         EXTI_SetPolarity(EXTI_EINT2, EXTI_POLARITY_LOW_ACTIVE_OR_FALLING_EDGE);
     }
     else
     {
-        // Activar interrupciones
-        // Configurar flanco ascendente
     	NVIC_DisableIRQ(EINT3_IRQn);
+    	NVIC_EnableIRQ(EINT0_IRQn);
         EXTI_SetPolarity(EXTI_EINT2, EXTI_POLARITY_HIGH_ACTIVE_OR_RISING_EDGE);
     }
-
-    // uint32_t active_int = NVIC->ISER[0]; /* Read the active interrupts */
-
-    // if ((active_int & (1 << EINT0_IRQn)))
-    // { /* If the external interrupt 0 is active */
-    //     NVIC_DisableIRQ(EINT0_IRQn);
-    //     NVIC_DisableIRQ(EINT1_IRQn);
-    //     NVIC_DisableIRQ(TIMER0_IRQn);
-    //     SYSTICK_IntCmd(DISABLE);
-    //     /* Disable the controls*/
-    // }
-    // else
-    // {
-    //     NVIC_EnableIRQ(EINT0_IRQn);
-    //     NVIC_EnableIRQ(EINT1_IRQn);
-    //     NVIC_EnableIRQ(TIMER0_IRQn);
-    //     SYSTICK_IntCmd(ENABLE);
-    //     /* Enable the controls */
-    // }
 
 }
 
@@ -73,13 +81,15 @@ void EINT3_IRQHandler(void)
     EXTI_ClearEXTIFlag(EXTI_EINT3); /* Clear the external interrupt flag */
     if(LPC_SC->EXTPOLAR & (1 << 3))
     {
-        LPC_GPIO0->FIOSET = MOTOR_POS_PIN; /* Turn on the motor */
+        LPC_GPIO0->FIOSET |= MOTOR_POS_PIN; /* Turn on the motor */
+        LPC_GPIO0->FIOCLR |= MOTOR_NEG_PIN;
         // Configurar flanco descendente
         EXTI_SetPolarity(EXTI_EINT3, EXTI_POLARITY_LOW_ACTIVE_OR_FALLING_EDGE);
     }
     else
     {
-        LPC_GPIO0->FIOCLR = MOTOR_POS_PIN; /* Turn on the motor */
+        LPC_GPIO0->FIOSET |= MOTOR_NEG_PIN; /* Turn on the motor */
+        LPC_GPIO0->FIOCLR |= MOTOR_POS_PIN;
         // Configurar flanco ascendente
         EXTI_SetPolarity(EXTI_EINT3, EXTI_POLARITY_HIGH_ACTIVE_OR_RISING_EDGE);
     }
@@ -98,7 +108,7 @@ int main(void)
 
     config_eint(); /* Configure the external interrupt */
 
-    // config_timer_and_match(); /* Configure the timer and match */
+    config_timer_and_match(); /* Configure the timer and match */
 
     // battery_control(); /* Call the battery control function */
 
@@ -106,7 +116,7 @@ int main(void)
 
     // SYSTICK_Cmd(ENABLE); /* Enable the systick timer */
 
-    // NVIC_EnableIRQ(EINT0_IRQn); /* Enable the external interrupt 0 */
+    NVIC_EnableIRQ(EINT0_IRQn); /* Enable the external interrupt 0 */
 
     // NVIC_EnableIRQ(EINT1_IRQn); /* Enable the external interrupt 1 */
 
@@ -116,15 +126,15 @@ int main(void)
 
     // NVIC_EnableIRQ(TIMER0_IRQn); /* Enable the timer 0 */
 
-    NVIC_SetPriority(EINT2_IRQn, HIGH_PRIORITY); /* Set the priority of the external interrupt 0 */
+    NVIC_SetPriority(EINT2_IRQn, MID_PRIORITY); /* Set the priority of the external interrupt 0 */
 
-    // NVIC_SetPriority(EINT0_IRQn, MID_PRIORITY); /* Set the priority of the external interrupt 1 */
+    NVIC_SetPriority(EINT0_IRQn, MID_PRIORITY); /* Set the priority of the external interrupt 1 */
 
     // NVIC_SetPriority(EINT1_IRQn, MID_PRIORITY); /* Set the priority of the external interrupt 2 */
 
     NVIC_SetPriority(EINT3_IRQn, MID_PRIORITY); /* Set the priority of the external interrupt 0 */
 
-    // NVIC_SetPriority(TIMER0_IRQn, LOW_PRIORITY); /* Set the priority of the timer 0 */
+    NVIC_SetPriority(TIMER0_IRQn, HIGH_PRIORITY); /* Set the priority of the timer 0 */
 
     while (TRUE) /* Infinite loop */
     {

0ce4d170dd7bc4b08ce2aae124d82578af05e83e
Sofia Aldana Ávalos <108819650+sofiavalos@users.noreply.github.com>
2024-11-14 21:37:49 -0300

    Update configuration.c

--- src/configuration.c
+++ src/configuration.c
@@ -1,113 +1,46 @@
-#include "configuration.h"
+#pragma once
 
-void config_ports(void)
-{
+#include "lpc17xx_exti.h"    /* Include the EXTI library */
+#include "lpc17xx_gpio.h"    /* Include the GPIO library */
+#include "lpc17xx_pinsel.h"  /* Include the PINSEL library */
+#include "lpc17xx_timer.h"   /* Include the TIMER library */
 
-    PINSEL_CFG_Type pin_cfg; /**< Pin configuration structure */
+#define MOTOR_NEG_PIN           ((uint32_t)(1 << 6)) /**< Assign port 0, pin 22 to the LED pin */
+#define MOTOR_POS_PIN         ((uint32_t)(1 << 3))  /**< Assign port 0, pin 3 to the motor pin */
+#define MANUAL_SWITCH_PIN ((uint32_t)(1 << 13)) /**< Assign port 2, pin 12 to the manual switch pin */
+#define OVERRIDE_SWITCH_PIN   ((uint32_t)(1 << 12)) /**< Assign port 2, pin 13 to the door switch pin */
+#define ALARM_PIN           ((uint32_t) (1<<2)) /**< Assign port 0 pin 2 to the alarm pin */
+#define FIRE_SENSOR_PIN   ((uint32_t) (1<<10))
 
-    pin_cfg.Portnum = PINSEL_PORT_0;           /**< Assign port 0 to the pin configuration */
-    pin_cfg.Pinnum = PINSEL_PIN_22;            /**< Assign pin 22 to the pin configuration */
-    pin_cfg.Funcnum = PINSEL_FUNC_0;           /**< Assign function 0 to the pin configuration */
-    pin_cfg.Pinmode = PINSEL_PINMODE_PULLUP;   /**< Assign pull-up mode to the pin configuration */
-    pin_cfg.OpenDrain = PINSEL_PINMODE_NORMAL; /**< Assign normal mode to the pin configuration */
+#define INPUT  0 /**< Macro to define the input */
+#define OUTPUT 1 /**< Macro to define the output */
 
-    PINSEL_ConfigPin(&pin_cfg); /**< Configure the pin */
+#define TRUE  1 /**< Macro to define the true */
+#define FALSE 0 /**< Macro to define the false */
 
-    /* Configure the positive motor pin */
+#define HIGH_PRIORITY 0 /**< Define the high priority */
+#define MID_PRIORITY  1 /**< Define the low priority */
+#define LOW_PRIORITY  2 /**< Define the low priority */
 
-    pin_cfg.Pinnum = PINSEL_PIN_3; /** Assign pin 3 to the pin configuration */
-    PINSEL_ConfigPin(&pin_cfg);    /** Configure the pin */
-
-    /* Configure the motor negative pin */
-    pin_cfg.Pinnum = PINSEL_PIN_6; /** Assign pin 6 to the pin configuration */
-    PINSEL_ConfigPin(&pin_cfg);    /** Configure the pin */
-
-    pin_cfg.Pinnum = PINSEL_PIN_2;   /** Assign pin 2 to the pin configuration */
-    PINSEL_ConfigPin(&pin_cfg); /** Configure the pin */
-
-    pin_cfg.Portnum = PINSEL_PORT_2; /** Assign port 2 to the pin configuration */
-    pin_cfg.Pinnum = PINSEL_PIN_12;  /** Assign pin 12 to the pin configuration */
-    pin_cfg.Funcnum = PINSEL_FUNC_1; /** Assign function 1 to the pin configuration */
-
-    PINSEL_ConfigPin(&pin_cfg); /** Configure the pin */
-
-    /* Configure the switch low pin */
-
-    pin_cfg.Pinnum = PINSEL_PIN_10; /** Assign pin 10 to the pin configuration */
-
-    PINSEL_ConfigPin(&pin_cfg); /** Configure the pin */
-
-    pin_cfg.Pinnum = PINSEL_PIN_11; // EINT1
-    PINSEL_ConfigPin(&pin_cfg); /** Configure the pin */
-
-    /* Configure the switch high pin */
-
-    pin_cfg.Pinnum = PINSEL_PIN_13; /** Assign pin 11 to the pin configuration */
-
-    PINSEL_ConfigPin(&pin_cfg); /** Configure the pin */
-
-    GPIO_SetDir(PINSEL_PORT_0, (MOTOR_NEG_PIN | MOTOR_POS_PIN | ALARM_PIN), OUTPUT); /* Set the LED and motor pins as output */
-    // GPIO_SetDir(PINSEL_PORT_0, I_TEMPERATURE_PIN, INPUT);          /* Set the temperature sensor pin as input */
-    GPIO_SetDir(PINSEL_PORT_2, (MANUAL_SWITCH_PIN | OVERRIDE_SWITCH_PIN | FIRE_SENSOR_PIN),
-                INPUT); /* Set the switch pins as input */
-}
-
-void config_eint(void)
-{
-    EXTI_InitTypeDef eint_init_struct; /**< Define the external interrupt initialization structure */
-
-    eint_init_struct.EXTI_Line =
-         EXTI_EINT0; /** Assign external interrupt 0 to the external interrupt initialization structure */
-    eint_init_struct.EXTI_Mode =
-        EXTI_MODE_EDGE_SENSITIVE; /** Assign edge sensitivity to the external interrupt initialization structure */
-    eint_init_struct.EXTI_polarity =
-        EXTI_POLARITY_HIGH_ACTIVE_OR_RISING_EDGE; /** Assign high active or rising edge sensitivity to the external
-                                                     interrupt initialization structure */
-
-    EXTI_Config(&eint_init_struct); /** Initialize the external interrupt */
-
-    // eint_init_struct.EXTI_Line =
-    //     EXTI_EINT1; /** Assign external interrupt 1 to the external interrupt initialization structure */
-
-    // EXTI_Config(&eint_init_struct); /** Initialize the external interrupt */
-
-    eint_init_struct.EXTI_Line =
-        EXTI_EINT2; /** Assign external interrupt 2 to the external interrupt initialization structure */
-
-    EXTI_Config(&eint_init_struct); /** Initialize the external interrupt */
-
-    eint_init_struct.EXTI_Line =
-        EXTI_EINT3; /** Assign external interrupt 3 to the external interrupt initialization structure */
-
-    EXTI_Config(&eint_init_struct); /** Initialize the external interrupt */
-
-    eint_init_struct.EXTI_Line = EXTI_EINT1;
-    EXTI_Config(&eint_init_struct); /** Initialize the external interrupt */
-}
+#define PRESCALE_VALUE 1000 /**< Define the prescale value */
+#define MATCH_CHANNEL_0 0
+#define MATCH_CHANNEL_1 1
+#define MATCH_VALUE 5000
 
 
-void config_timer_and_match(void){
-    TIM_TIMERCFG_Type timer_cfg; /**< Define the timer configuration structure */
+/**
+ * @brief Configures the ports for the temperature sensor, switch, LED and motor
+ *
+ * @return void
+ */
+void config_ports(void);
 
-    timer_cfg.PrescaleOption = TIM_PRESCALE_USVAL; /** Assign the tick value to the prescale option */
-    timer_cfg.PrescaleValue = PRESCALE_VALUE;        /** Assign the prescale value to the prescale option */
+/**
+ * @brief Configures the external interrupt
+ *
+ * @return void
+ */
+void config_eint(void);
 
-    TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &timer_cfg); /** Initialize the timer */
 
-
-    TIM_MATCHCFG_Type match_cfg; /**< Define the match configuration structure */
-
-    match_cfg.MatchChannel = MATCH_CHANNEL; /** Assign match channel 0 to the match configuration structure */
-    match_cfg.IntOnMatch = ENABLE; /** Enable the interrupt on match */
-    match_cfg.ResetOnMatch = ENABLE; /** Enable the reset on match */
-    match_cfg.StopOnMatch = DISABLE; /** Disable the stop on match */
-    match_cfg.ExtMatchOutputType = TIM_EXTMATCH_TOGGLE; /** Assign the external match output type to the match
-                                                             configuration structure */
-    match_cfg.MatchValue = (uint32_t)MATCH_VALUE; /** Assign the match value to the match configuration structure */
-
-    TIM_ConfigMatch(LPC_TIM0, &match_cfg); /** Configure the match */
-
-    TIM_Cmd(LPC_TIM0, ENABLE); /** Enable the timer */
-    NVIC_EnableIRQ(TIMER0_IRQn); /** Enable the timer 0 interrupt */
-}
-
+void config_timer_and_match(void);
